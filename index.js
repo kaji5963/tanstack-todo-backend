02a394bd55f53b5cde20eb260a7ce8e9e39c6c37
takuya_kajiwara <fd.elt.119@gmail.com>
2023-12-19 22:46:03 +0900

    ✨ TODO追加機能実装

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ const port = 3001;
 
 app.use(cors());
 
+app.use(express.json());
+
 const prisma = new PrismaClient();
 
 app.get('/', (req, res) => res.send('Hello World'));
@@ -18,6 +20,24 @@ app.get('/todos', async (req, res) => {
   const todos = await prisma.todo.findMany();
 
   return res.json(todos);
+});
+
+// 一時的にエラーとなるように返す
+// app.get('/todos', async (req, res) => {
+//   return res.status(500).json({ message: 'Internal Server Error From Server' });
+// });
+
+app.post('/todos/create', async (req, res) => {
+  const { name } = req.body;
+
+  const todo = await prisma.todo.create({
+    data: {
+      name,
+      isCompleted: false,
+    },
+  });
+
+  return res.json(todo);
 });
 
 app.listen(port, () => console.log(`Server Listen on Port ${port}`));
